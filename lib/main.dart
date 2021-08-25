97970c9612d0b2774826ee256ba50e96c36cfc7a
Mohammad Bilal <mdbilal7620@gmail.com>
2021-08-25 17:08:33 +0530

    initial commit!

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import './transactions.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(myApp());
+final List<transactions> transactionLists = [
+  transactions('t1', 'Shoes', 5000, DateTime.now()),
+  transactions('t2', 't-shirts', 500, DateTime.now()),
+  transactions('t3', 'Watch', 1500, DateTime.now()),
+  transactions('t4', 'Glasses', 800, DateTime.now()),
+  transactions('t5', 'Potatoes', 100, DateTime.now()),
+];
+
+class myApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Extrack",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Extrack"),
+        ),
+        body: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              child: Card(
+                color: Colors.deepOrangeAccent,
+                child: Text("CHART"),
+                elevation: 7,
+              ),
+            ),
+            Column(
+              children: transactionLists.map((e) {
+                return Card(
+                  child: Row(
+                    children: [
+                      Container(
+                        child: Text(e.amount.toString()),
+                      ),
+                      Column(
+                        children: [
+                          Text(e.title),
+                          Text(e.date.toString()),
+                        ],
+                      )
+                    ],
+                  ),
+                );
+              }).toList(),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/transactions.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+
+class transactions {
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime date;
+
+  transactions(@required this.id, @required this.title, @required this.amount,
+      @required this.date);
+}
